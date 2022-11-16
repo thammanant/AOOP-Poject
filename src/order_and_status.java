61dc1529a5a667363b3766a193d6485bde92ae7d
jimmy666k <brightacs74@gmail.com>
2022-11-16 16:43:04 +0700

    Order and status also order detail updates

--- src/order_and_status.java
+++ src/order_and_status.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class order_and_status {
     private JButton Basket_button;
     private JPanel Basket_panel;
+    private JButton Status;
 
 
     public order_and_status(JFrame frame){
+
 
     }
     public JPanel getOrder_and_statusPanel(){
