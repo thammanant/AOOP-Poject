87e79a51e0119506e9717e866d8585dc4232c101
Puttiphat U <90830272+Puttiphat-M@users.noreply.github.com>
2022-11-17 04:43:10 +0700

    Add back button

--- src/Report.java
+++ src/Report.java
@@ -5,12 +5,21 @@ import java.awt.event.ActionListener;
 public class Report {
     private JPanel ReportPanel;
     private JButton Exit_button;
+    private JButton Back_button;
 
     public Report(JFrame frame){
         Exit_button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 frame.setContentPane(new Home(frame).getHomePanel());
+                frame.revalidate();
+            }
+        });
+
+        Back_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setContentPane(new Other(frame).getOtherPanel());
                 frame.revalidate();
             }
         });
