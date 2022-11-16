4d0f8ab60144fe42b3156dd74793ab207969c0a3
Pun <80048553+thammanant@users.noreply.github.com>
2022-11-16 16:31:31 +0700

    Navigate

--- src/Others.java
+++ src/Others.java
@@ -2,4 +2,12 @@ import javax.swing.*;
 
 public class Others {
     private JPanel OtherPanel;
+
+    public Others(){
+
+    }
+
+    public JPanel getOtherPanel(){
+        return OtherPanel;
+    }
 }
