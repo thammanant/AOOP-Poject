c9282f65ef3b773fb8cd51f05dafd70ef11fe590
jimmy666k <brightacs74@gmail.com>
2022-11-17 04:18:39 +0700

    Report

---
+++ src/Report.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Report {
+    private JPanel Report_panel;
+    private JTextArea reportTextArea;
+    private JButton Send_button;
+
+    public Report(JFrame frame){
+
+    }
+
+    public JPanel get_Report_panel(){
+        return Report_panel;
+    }
+}
