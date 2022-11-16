60d47cab6261089bffc11e8096919881c92fd5d5
Puttiphat U <90830272+Puttiphat-M@users.noreply.github.com>
2022-11-17 04:42:52 +0700

    Add back button

--- src/Order_detail.java
+++ src/Order_detail.java
@@ -7,12 +7,20 @@ public class Order_detail {
     private JLabel Profile_icon;
     private JButton Chat_icon;
     private JButton exit_button;
+    private JButton Back_button;
 
     public Order_detail(JFrame frame) {
         exit_button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 frame.setContentPane(new Home(frame).getHomePanel());
+                frame.revalidate();
+            }
+        });
+        Back_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setContentPane(new Other(frame).getOtherPanel());
                 frame.revalidate();
             }
         });
