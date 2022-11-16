56410154f83cc56ec6c75221ac325bffcf517b0a
Puttiphat U <90830272+Puttiphat-M@users.noreply.github.com>
2022-11-17 04:41:37 +0700

    Change History to Other button

--- src/Home.java
+++ src/Home.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -7,20 +6,12 @@ public class Home {
 
     private JButton HomeButton;
     private JButton NewOrderButton;
-    private JButton ChatButton;
+    private JButton OtherButton;
     private JPanel HomePanel;
-    private JButton History_Button;
-    private JButton Report_Button;
+    private JButton Home_Button;
 
     public Home(JFrame frame) {
-        //set Button home
-        HomeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frame.setContentPane(new Home(frame).getHomePanel());
-                frame.revalidate();
-            }
-        });
+
         // set new order button
         NewOrderButton.addActionListener(new ActionListener() {
             @Override
@@ -31,18 +22,18 @@ public class Home {
         });
 
         //set the other button
-        History_Button.addActionListener(new ActionListener() {
+        Home_Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                frame.setContentPane((new History(frame).getHistoryPanel()));
+                frame.setContentPane((new Home(frame).getHomePanel()));
                 frame.revalidate();
             }
         });
 
-        Report_Button.addActionListener(new ActionListener() {
+        OtherButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                frame.setContentPane((new Report(frame).getReportPanel()));
+                frame.setContentPane((new Other(frame).getOtherPanel()));
                 frame.revalidate();
             }
         });
