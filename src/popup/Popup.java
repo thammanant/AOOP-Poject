43d7e09468ecc53b3a084a0f490f37d4ca77d4c9
jimmy666k <brightacs74@gmail.com>
2022-11-17 01:52:55 +0700

    Popup

---
+++ src/popup/Popup.java
@@ -0,0 +1,10 @@
+package popup;
+
+import javax.swing.*;
+
+public class Popup {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null,"Your order have been sent","Message",JOptionPane.PLAIN_MESSAGE);
+    }
+
+}
