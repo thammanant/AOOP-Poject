fde4eaf23fd49adef24deb38cf4322027099fdd2
jimmy666k <brightacs74@gmail.com>
2022-11-17 05:20:55 +0700

    Report last edit

--- src/Report.java
+++ src/Report.java
@@ -1,11 +1,30 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Report {
     private JPanel Report_panel;
     private JTextArea reportTextArea;
     private JButton Send_button;
+    private JButton exit_button;
+    private JButton Back_button;
 
     public Report(JFrame frame){
+        exit_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setContentPane(new Home(frame).getHomePanel());
+                frame.revalidate();
+            }
+        });
+
+        Back_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setContentPane(new Other(frame).getOtherPanel());
+                frame.revalidate();
+            }
+        });
 
     }
 
