cc9f05259d18d1745df2b1af46d8e286f4febfac
jimmy666k <brightacs74@gmail.com>
2022-11-21 20:24:10 +0700

    update UI (Worker status)

--- src/Main.java
+++ src/Main.java
@@ -134,8 +134,12 @@ public class Main {
 
         Customer customer = new Customer();
         customer.setPhone("None");
-        Login login = new Login(frame, customer, new Worker());
-        frame.getContentPane().add(login.getLoginPanel());
+
+        worker_status test = new worker_status(frame,new Worker());
+        frame.getContentPane().add(test.getworker_status());
+
+//        Login login = new Login(frame, customer, new Worker());
+//        frame.getContentPane().add(login.getLoginPanel());
 
     }
 }
