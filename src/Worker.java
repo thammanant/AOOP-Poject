24840658e3de4e0bddf9cc76e299cdda469d094b
Puttiphat U <90830272+Puttiphat-M@users.noreply.github.com>
2022-11-22 21:51:31 +0700

    correct the order function

--- src/Worker.java
+++ src/Worker.java
@@ -25,17 +25,22 @@ public class Worker extends User {
     }
     //check order of customer
     public String checkOrder() throws JacksonUtilityException, FirebaseException, IOException {
-        String cus[] = new String[DataBaseFB.findAllCustomerUsernames().size()];
+        String cus[] = new String[DataBaseFB.findAllCustomerUsernames().size()];            //create customers
         String res = "";
-        for(int i =0 ; i< DataBaseFB.findAllCustomerUsernames().size();i++){
+        for(int i =0 ; i< DataBaseFB.findAllCustomerUsernames().size();i++){                //transfer all customers to array
             cus[i] = DataBaseFB.findAllCustomerUsernames().get(i);
         }
         for(int i=0; i< cus.length;i++){
-            if(DataBaseFB.getHistory(cus[i],i+1).get(15).equals("-3")){
-                res = "You don't have order now";
-            }
-            else{
-                res = "You have order now";
+            int s =DataBaseFB.getHistoryAmount(cus[i]);
+            for(int j=0; j < s;j++) {
+                if (DataBaseFB.getHistory(cus[i], j + 1).get(15).equals("-3")) {
+                    res = "You don't have order now";
+                    return res;
+                }
+                else {
+                    res = "You have order now";
+                    return res;
+                }
             }
         }
         return res;
